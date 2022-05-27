447230ca6febee4c01654247946f959981104fac
Tanvir Ahmed <tanvir.be.global@gmail.com>
2022-05-27 08:50:52 +0600

    update login and singin

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,9 +1,13 @@
 import React from "react";
+import { FcGoogle } from "react-icons/fc";
+import { Link } from "react-router-dom";
 
 const Login = () => {
   return (
     <section className="min-h-[91%] bg-accent">
-      <h2 className="text-white text-3xl text-center mt-5 font-semibold">Login</h2>
+      <h2 className="text-white text-3xl text-center mt-5 font-semibold">
+        Login
+      </h2>
 
       <div className="">
         <form action="flex justify-center items-center">
@@ -31,8 +35,22 @@ const Login = () => {
               class="input w-full max-w-xs text-white btn btn-primary"
               value="Loin"
             />
+              <div className="text-white font-semibold text-sm">
+                <Link to="">Forgotten password?</Link>
+              </div>
+              <div className="text-white font-semibold text-sm">
+                <Link to="">Create new account?</Link>
+              </div>
+
+            <div class="w-full max-w-xs text-white divider mx-auto">OR</div>
           </div>
         </form>
+        <div className="w-full">
+          <button className="flex justify-center items-center  px-5 py-3 bg-blue-100 text-xl text-black font-semibold rounded-lg mx-auto max-w-xs w-full">
+            <FcGoogle className="text-3xl mr-3" />
+            <span>Sing IN with Google</span>
+          </button>
+        </div>
       </div>
     </section>
   );
