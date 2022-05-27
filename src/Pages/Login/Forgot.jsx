75b15a40220a5906343f5f49511ad9f4ac787cd2
Tanvir Ahmed <tanvir.be.global@gmail.com>
2022-05-27 23:52:41 +0600

    added forgot page

--- src/Pages/Login/Forgot.jsx
+++ src/Pages/Login/Forgot.jsx
@@ -1,22 +1,24 @@
 import React from "react";
 import { useSendPasswordResetEmail } from "react-firebase-hooks/auth";
+import { useForm } from "react-hook-form";
 import { toast } from "react-toastify";
 import auth from "../../fierbase.init";
 
 const Forgot = () => {
     const [sendPasswordResetEmail, sending, error] = useSendPasswordResetEmail(auth);
+    const { register, handleSubmit, reset } = useForm();
 
-    const handleResetMail = e =>{
-        e.preventDefault()
-        const email = e.target.email.value
-        sendPasswordResetEmail(email)
+    const onSubmit = data =>{
+        sendPasswordResetEmail(data.email)
+        reset()
         if(!error){
-            toast.success('Reset Email Send')
+            toast.success("Reset Email Send")
         }
     }
+
   return (
     <div className="flex justify-center items-center h-[80%]">
-      <form onSubmit={handleResetMail} className="bg-accent p-8 rounded-xl w-96">
+      <form onSubmit={handleSubmit(onSubmit)} className="bg-accent p-8 rounded-xl w-96">
         <div className="flex flex-col items-center gap-5 w-full">
             <h2 className="text-white text-xl text-center font-semibold">Forgotten password</h2>
           <input
@@ -25,6 +27,7 @@ const Forgot = () => {
             placeholder="Enter your Email"
             class="input w-full"
             required
+            {...register("email")}
           />
           <input
             type="submit"
