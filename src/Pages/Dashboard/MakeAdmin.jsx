67cd37336ef56b38788c70e78372e32de7613501
Tanvir Ahmed <tanvir.be.global@gmail.com>
2022-05-30 23:17:28 +0600

    delete and make admin

--- src/Pages/Dashboard/MakeAdmin.jsx
+++ src/Pages/Dashboard/MakeAdmin.jsx
@@ -42,10 +42,43 @@ const MakeAdmin = () => {
     });
   };
 
+
+
+
+
+  const handelUserDelete = (id) => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        fetch(`http://localhost:5000/userDelete/${id}`, {
+          method: "DELETE",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        })
+          .then((req) => req.json())
+          .then((data) => {
+            setReFacth(!refacth)
+            if (data) {
+                
+              Swal.fire("Delete!", "Your delete.", "success");
+            }
+          });
+      }
+    });
+  };
+
   return (
     <div className="p-5">
       <h1 className="text-2xl text-center font-semibold mt-5 my-8">
-        Manage Product
+        Make Admin
       </h1>
 
       <div className="overflow-x-auto">
@@ -89,7 +122,7 @@ const MakeAdmin = () => {
                       Remove User
                     </button>
                   ) : (
-                    <button className="btn btn-sm bg-[#fc2323] hover:bg-[#636161]">
+                    <button onClick={()=> handelUserDelete(user?._id)} className="btn btn-sm bg-[#fc2323] hover:bg-[#636161]">
                       Remove User
                     </button>
                   )}
@@ -97,27 +130,6 @@ const MakeAdmin = () => {
               </tr>
             ))}
 
-            {/* {allProductData.map((pd, index) => (
-              <tr>
-                {console.log(pd)}
-                <th>{index + 1}</th>
-                <th className="w-11 w-11 overflow-hidden">
-                  {" "}
-                  <img src={pd?.pdImg} alt="img" />
-                </th>
-                <td>{pd?.pdName}</td>
-                <td>{pd?.addProductEmail}</td>
-                <td>{pd?.availablePd}</td>
-                <td>
-                  <button
-                    onClick={() => handelDelete(pd?._id)}
-                    className="btn bg-[#bd0000] hover:bg-[#ff0202] btn-sm "
-                  >
-                    Delete
-                  </button>
-                </td>
-              </tr>
-            ))} */}
           </tbody>
         </table>
       </div>
